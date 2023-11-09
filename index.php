51403d64da32991820b435bd5ad60763bc8727f0
Nicolas-Jaime <nicolas.jaime00@gmail.com>
2023-11-08 22:44:53 -0300

    COMPONENTICÉ GENTE WOOOOW :v 😎

--- index.php
+++ index.php
@@ -30,36 +30,9 @@
         if($_SESSION['cargo'] == 1){
         ?>
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#!">wopaaaaaaaaaaaaaaaaaaaa</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link" href="logout.php">log out</a></li>
-                    </ul>
-                    <form class="d-flex">
-                        <li class="nav-link dropdown">
-                            <a class="btn btn-outline-dark dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi-cart-fill me-1"></i>
-                            Cart
-                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span></a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                
-                            
-                            
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
-                                <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
-
-
-                                
-                            </ul>
-                        </li>
-                    </form>
-                </div>
-            </div>
-        </nav>
+        <?php
+            require_once "components/navbar.php";
+        ?>
         <!-- Header-->
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
@@ -108,36 +81,9 @@
         }else if($_SESSION['cargo'] == 2){
         ?>
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#!">wopaaaaaaaaaaaaaaaaaaaa</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link" href="logout.php">log out</a></li>
-                    </ul>
-                    <form class="d-flex">
-                        <li class="nav-link dropdown">
-                            <a class="btn btn-outline-dark dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi-cart-fill me-1"></i>
-                            Cart
-                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span></a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                
-                            
-                            
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
-                                <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
-
-
-                                
-                            </ul>
-                        </li>
-                    </form>
-                </div>
-            </div>
-        </nav>
+        <?php
+            require_once "components/navbar.php";
+        ?>
         <!-- Header-->
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
@@ -192,37 +138,9 @@
         else{
         ?>
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#!">wopaaaaaaaaaaaaaaaaaaaa</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link" href="login.php">Log in</a></li>
-                        <li class="nav-item"><a class="nav-link" href="signin.php">Sign in</a></li>
-                    </ul>
-                    <form class="d-flex">
-                        <li class="nav-link dropdown">
-                            <a class="btn btn-outline-dark dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi-cart-fill me-1"></i>
-                            Cart
-                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span></a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                
-                            
-                            
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
-                                <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
-
-
-                                
-                            </ul>
-                        </li>
-                    </form>
-                </div>
-            </div>
-        </nav>
+        <?php
+            require_once "components/navbar.php";
+        ?>
         <!-- Header-->
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
